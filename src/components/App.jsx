8b4a46af53b9be151950f228c5282d81cb321901
Farshad <farshad.basereh@gmail.com>
2019-12-31 16:44:36 -0500

    insert conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Login from "./login";
+
+let isLoggedIn = true;
+
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {
+        isLoggedIn ? <h1>Hello</h1> : <Login />
+      }
     </div>
   );
 }
